a8b1c1cd43cb08fd04d7aa49e04409299a468f72
Norton794 <nortonsantos79@gmail.com>
2022-04-13 22:03:45 -0300

    feat: Started Advice component style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Advice from './components/Advice';
+
+function App() {
+  return (
+    <div className="App">
+      <Advice/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Advice.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Advice(props) {
+  return (
+    <div className="main">
+      <div className="advice">
+        <p className="quote">teste</p>
+      </div>
+    </div>
+  );
+}
