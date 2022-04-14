97c0620a029791fe7bf1097b10e7758592e372c4
Norton794 <nortonsantos79@gmail.com>
2022-04-13 22:40:07 -0300

    feat: desktop style done

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,10 +1,27 @@
 import react from "react";
+import AdviceNumber from "./AdviceNumber";
 
 export default function Advice(props) {
   return (
     <div className="main">
       <div className="advice">
-        <p className="quote">teste</p>
+        <AdviceNumber text="ADVICE #117" />
+        <p className="quote">
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </p>
+
+        <img
+          src="/img/pattern-divider-desktop.svg"
+          alt="pattern-divider"
+          style={{ marginTop: "20px" }}
+        />
+
+        <div className="dice-ball">
+          <div className="dice">
+            <img src="/img/icon-dice.svg" alt="dice" />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/AdviceNumber.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import "./AdviceNumber.css";
+export default (props) => {
+  return (
+    <div>
+      <h6 className="advice-number">{props.text}</h6>
+    </div>
+  );
+};
